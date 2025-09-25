496e28bfce072c37cf24854985dd4aba1042e9f3
REHAN-NASEER <rehannaseer32123@gmail.com>
2025-09-25 21:03:43 +0500

    Added the functionality of the splash screen , world Stats & country wise Stats

--- lib/Screens/countriesListScreen.dart
+++ lib/Screens/countriesListScreen.dart
@@ -1,4 +1,7 @@
+import 'package:covidtracker/Services/fetchWorldStats.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
 class Countrieslistscreen extends StatefulWidget {
   const Countrieslistscreen({super.key});
 
@@ -7,8 +10,129 @@ class Countrieslistscreen extends StatefulWidget {
 }
 
 class _CountrieslistscreenState extends State<Countrieslistscreen> {
+  TextEditingController searchController = TextEditingController();
+  FetchWorldStats fetchWorldStats = FetchWorldStats();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                onChanged: (value){
+                  setState(() {
+
+                  });
+                },
+                controller: searchController,
+                decoration: InputDecoration(
+                  hintText: 'Search with country name ',
+                  contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: FutureBuilder(
+                future: fetchWorldStats.fetchCountryRecords(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return Shimmer.fromColors(
+                          // period: Duration(milliseconds: 700),
+                          baseColor: Colors.grey.shade700,
+                          highlightColor: Colors.grey.shade100,
+                          child: ListTile(
+                            title: Container(
+                              width: 80,
+                              height: 10,
+                              color: Colors.white,
+                            ),
+                            subtitle: Container(
+                              width: 80,
+                              height: 10,
+                              color: Colors.white,
+                            ),
+                            leading: Container(
+                              width: 50,
+                              height: 50,
+                              color: Colors.white,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        String name = snapshot.data![index]['country'];
+                        if(searchController.text.isEmpty){
+                          return ListTile(
+                            title: Text(
+                              snapshot.data![index]['country'],
+                              style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 15,
+                              ),
+                            ),
+                            subtitle: Text(
+                              snapshot.data![index]['cases'].toString(),
+                            ),
+                            leading: Image(
+                              width: 50,
+                              height: 50,
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                snapshot.data![index]['countryInfo']['flag'],
+                              ),
+                            ),
+                          );
+                        }else if(name.toLowerCase().contains(searchController.text.toLowerCase())){
+                          return ListTile(
+                            title: Text(
+                              snapshot.data![index]['country'],
+                              style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 15,
+                              ),
+                            ),
+                            subtitle: Text(
+                              snapshot.data![index]['cases'].toString(),
+                            ),
+                            leading: Image(
+                              width: 50,
+                              height: 50,
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                snapshot.data![index]['countryInfo']['flag'],
+                              ),
+                            ),
+                          );
+                        }else{
+                          return Container();
+                        }
+
+
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/Services/fetchWorldStats.dart
+++ lib/Services/fetchWorldStats.dart
@@ -15,4 +15,15 @@ class FetchWorldStats {
       throw Exception('Error');
     }
   }
+
+  Future<List<dynamic>> fetchCountryRecords() async {
+    final response = await http.get(Uri.parse(AppUrl.countriesList));
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception('Error');
+    }
+  }
 }
